c05edc055f931ede617ba91a8ce6a7af93101abb
Callista Chi <callistarchi@gmail.com>
2018-08-14 07:51:18 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,16 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-    puts 
+    puts "#{person[0]}: #{colors}"
 end
+
+red = ['R', 'red']
+orange = ['O', 'orange']
+yellow = ['Y', 'yellow']
+green = ['G', 'green']
+blue = ['B', 'blue']
+indigo = ['I', 'indigo']
+violet = ['V', 'violet']
+
+
 
 colors = ['red','orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
